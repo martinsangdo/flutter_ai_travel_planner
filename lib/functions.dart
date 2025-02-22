819b718f20dabe1ae281dae7d7d4dee9d0c23212
Martin SangDo <martin.sangdo@gmail.com>
2025-02-22 04:15:02 +0000

    - parsed accommodations

--- lib/functions.dart
+++ lib/functions.dart
@@ -6,15 +6,21 @@ import 'dart:convert';
 import 'package:ai_travel_planner/constants.dart';
 import 'package:flutter/material.dart';
 
+/*
+return:
+- city
+- country
+- start date
+- currency
+- 
+*/
 Map<String, dynamic> parseRawTripDetails(rawData){
   Map<String, dynamic> results = {};
   int index = 0;
   for (dynamic item in rawData){
     if (item == ''){
+      index++;
       continue;
-    }
-    if (index > 20){
-      break;
     }
     //debugPrint('Index ======= ' + index.toString());
     if (!isPrimitive(item)){
@@ -23,10 +29,44 @@ Map<String, dynamic> parseRawTripDetails(rawData){
         //this is an array or object
         //debugPrint(item.length.toString());
       } else if (item is Map){
-        if (item['locationName'] != null){
-          results['locationName'] = rawData[item['locationName']];
+        if (item['cityMeta'] != null){
+          results['city'] = rawData[rawData[item['cityMeta']]['name']];
         }
+        if (item['countryMeta'] != null){
+          results['country'] = rawData[rawData[item['countryMeta']]['name']];
+        }
+        //begin date
+        if (item['travelAt'] != null){
+          results['travelAt'] = rawData[item['travelAt']];
+        }
+        //currency
+
+        //
+        if (item['description'] != null){
+          results['description'] = rawData[item['description']];
+        }
+        if (item['history'] != null){
+          results['history'] = rawData[item['history']];
+        }
+        //accommodation budget
+        if (item['budget'] != null){
+          int accommodationsBudgetIndex = item['budget'];
+          results['accommodations'] = [];
+          //get list of accommodations
+          Map acommodationMetaIndexes = rawData[accommodationsBudgetIndex];
+          debugPrint(acommodationMetaIndexes.toString());
+          for (int accommodationIndex in rawData[acommodationMetaIndexes['accommodations']]){
+            results['accommodations'].add({
+              'type': rawData[rawData[accommodationIndex]['type']],
+              'priceUsd': rawData[rawData[accommodationIndex]['priceUsd']]
+            });
+          }
+        }
+
       }
+    } else if (item is String){
+      //this can be key or value
+      
     }
     index++;
   }
